4662ab43616a283a875714671296a6c1f2731487
Christabel Khakali <khabetsachristabel@gmail.com>
2023-02-14 10:04:32 -0800

    SQL queries

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,11 +1,10 @@
---  Script that creates a table second_table in the database 
---  If the table second_table already exists, your script should not fail
---  You are not allowed to use the SELECT and SHOW statements
---  Script should create the given records
+-- create a table second_table in hbtn_0c_0
+-- add multiple rows
+CREATE TABLE IF NOT EXISTS second_table (id INT,
+name VARCHAR(256),
+score INT);
 
-CREATE TABLE IF NOT EXISTS second_table 
-	id INT, name VARCHAR(256), score INT);
-
-INSERT INTO `second_table` (`id`, `name`, `score`)
-
-VALUES (1, 'John', 10), (2, 'Alex', 3), (3, 'Bob', 14), (4, 'George', 8);
+INSERT INTO second_table(id, name, score) VALUES (1, 'John', 10);
+INSERT INTO second_table(id, name, score) VALUES (2, 'Alex', 3);
+INSERT INTO second_table(id, name, score) VALUES (3, 'Bob', 14);
+INSERT INTO second_table(id, name, score) VALUES (4, 'George', 8);
